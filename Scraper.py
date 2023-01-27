52f3db6ac9d73545755cbc2f5b2786376b8c1b1d
Alex <hirbu.alexandru@gmail.com>
2023-01-27 11:48:50 +0200

    app utility

--- Scraper.py
+++ Scraper.py
@@ -7,10 +7,20 @@ page = requests.get(url)
 
 if page.status_code == 200:
     soup = BeautifulSoup(page.content, 'html.parser')
+
+    headers = soup.find_all(['h1', 'h2', 'h3', 'h4', 'h5', 'h6'])
+    bodies = soup.find_all (['p', 'div'])
+    results = headers + bodies
+
+
     results = soup.find_all(string=lambda text: word in text)
 
     with open("results.txt", "w", encoding= 'utf-8') as f:
         for result in results:
-            f.write(result + '\n')
+            f.write(result.get_text() + '\n')
 else:
         print('Error')    
+
+
+
+   
